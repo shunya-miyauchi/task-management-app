dad9a9a82e81360cc44d169911136b56c8373343
Miyauchi <hy.miyauchi@gmail.com>
2022-02-22 16:28:51 +0900

    add index_view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,33 @@
 class TasksController < ApplicationController
+
+  # 一覧
+  def index
+    @tasks = Task.all
+  end
+
+  # 詳細
+  def show 
+
+  end
+
+  # 新規作成
+  def new
+
+  end
+
+  # 編集
+  def edit
+  end
+
+  def update
+  end
+
+  # 削除
+  def destroy
+  end
+
+  private
+
+
+
 end
