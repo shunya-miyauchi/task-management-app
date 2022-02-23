36dd0479ef516517fa0604add2c1a3b9bb7cfe96
Miyauchi <hy.miyauchi@gmail.com>
2022-02-23 21:23:46 +0900

    add:RSpec_test

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -12,17 +12,24 @@ RSpec.describe "タスク管理機能",type: :system do
       end      
     end        
   end
-
   describe "一覧表示機能" do
+    let!(:task) { FactoryBot.create(:task) }
+    let!(:task_second) { FactoryBot.create(:task,title:"課題",detail:"終わらせたい") }
+    before do
+      visit tasks_path
+    end
     context "一覧画面に遷移した場合" do
       it "作成済みのタスク一覧が表示される" do
-        FactoryBot.create(:task,title:"task")
-        visit tasks_path
-        expect(page).to have_content "task"
+        expect(page).to have_content "test_title"
       end
-    end        
+    end
+    context "タスクが作成日時の降順に並んでいる場合" do
+      it "新しいタスクが一番上に表示される" do
+        task_list = all(".task_list")
+        expect(task_list[0]).to have_content "課題"
+      end
+    end
   end
-
   describe "詳細表示機能" do
     context "任意のタスク詳細画面に遷移した場合" do
       it "該当タスクの内容が表示される" do
@@ -33,5 +40,4 @@ RSpec.describe "タスク管理機能",type: :system do
       end
     end        
   end
-  
 end
