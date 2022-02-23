8f435b87d5f3954e040d36cdcaa0011e75fc48c5
Miyauchi <hy.miyauchi@gmail.com>
2022-02-23 10:55:59 +0900

    fix:migration_file

--- db/migrate/20220222063008_change_column_to_task.rb
+++ db/migrate/20220222063008_change_column_to_task.rb
@@ -1,5 +1,5 @@
 class ChangeColumnToTask < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column :tasks, :name, :string, null: false
     change_column :tasks, :detail, :text, null: false
   end
