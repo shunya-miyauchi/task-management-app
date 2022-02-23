e027a4dd155f6ea16771b73ec78214d4dc5718bd
Miyauchi <hy.miyauchi@gmail.com>
2022-02-23 20:37:36 +0900

    add:time_zone:Tokyo

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module TaskManagementApp
                         request_specs: false
     end
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
