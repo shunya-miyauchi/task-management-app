7c90e45d2f2e6a2581a5f19638f9d61e428bd6e7
Miyauchi <hy.miyauchi@gmail.com>
2022-02-24 16:11:11 +0900

    add:tasks_titleカラムにindex追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_042056) do
+ActiveRecord::Schema.define(version: 2022_02_24_070530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_02_24_042056) do
     t.datetime "updated_at", null: false
     t.datetime "expired_at", default: -> { "now()" }, null: false
     t.string "status", default: "未着手", null: false
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
