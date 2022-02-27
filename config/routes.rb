d145ce74065ec1ddd037182bd63293139b545d47
Miyauchi <hy.miyauchi@gmail.com>
2022-02-27 17:39:18 +0900

    fix:sessionsルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
   resources :users , only: [:new,:create,:show]
+  resources :sessions , only: [:new,:create,:destroy]
 end
