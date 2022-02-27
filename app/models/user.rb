6351245cd634991a91a29e80597d2e0c58f9279b
Miyauchi <hy.miyauchi@gmail.com>
2022-02-27 17:04:41 +0900

    add:user_idとtasksの関連付け設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   before_validation { email.downcase! }
-  
+
   validates :name, presence: true, length: {maximum: 30}
   validates :email, presence: true, uniqueness: true, length: {maximum: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, length: {minimum: 6}
   
   has_secure_password
+  has_many :tasks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_27_074903) do
+ActiveRecord::Schema.define(version: 2022_02_27_075914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2022_02_27_074903) do
     t.datetime "expired_at", default: -> { "now()" }, null: false
     t.string "status", default: "未着手", null: false
     t.integer "priority", default: 0, null: false
+    t.bigint "user_id"
     t.index ["title"], name: "index_tasks_on_title"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2022_02_27_074903) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
