61c49a04798f9eb66d1a08abbc4dabf252861a7f
Miyauchi <hy.miyauchi@gmail.com>
2022-02-28 13:33:29 +0900

    add:generate_controller_admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
+  namespace :admin do
+    resources :users , only: [:index,:new,:create,:destroy]
+  end
   resources :users , only: [:new,:create,:show]
   resources :sessions , only: [:new,:create,:destroy]
 end
