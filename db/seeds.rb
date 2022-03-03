e30d0303d266dee981ef38329a3089661e5a1d73
Miyauchi <hy.miyauchi@gmail.com>
2022-03-03 20:33:32 +0900

    fix:seedデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,44 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# name = "みやうち"
-# email = "aaa@gmail.com"
-# password = "123456"
-# admin = true
+name = "みやうち"
+email = "aaa@gmail.com"
+password = "123456"
+admin = true
 
-3.times do |n|
+1.times do |n|
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    admin: admin,
+  )
+end
+
+9.times do |n|
+  User.create!(
+    name: "#{name}#{n+1}",
+    email: "#{n+1}#{email}",
+    password: password,
+    admin: "false",
+  )
+end
+
+10.times do |n|
 Label.create!(
   name: "ラベル#{n+1}",
   )
 end
 
-# 5.times do |n|
-#   User.create!(
-#     name: "#{name}#{n+1}",
-#     email: "#{n+1}#{email}",
-#     password: password,
-#     admin: "false",
-#   )
-# end
-
-# 10.times do |n|
-#   Task.create!(
-#     title: "課題#{n+1}",
-#     detail: "どんどん進めたい#{n+1}",
-#     status: "着手中",
-#     priority: "高",
-#     user_id: 21,
-#   )
-# end
+10.times do |n|
+  User.all.each_with_index do |user,idx|
+    Task.create!(
+      title: "課題#{n+1}",
+      detail: "どんどん進めたい#{n+1}",
+      status: "着手中",
+      priority: "高",
+      user_id: user.id,
+    )
+  end
+end
 
